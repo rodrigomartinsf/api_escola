5921b1a33bacf6dd78fb2d6e70f12fd6fbfba347
Rodrigo Martins <Rodrigo.MartinsFernandes@fit-tecnologia.org.br>
2022-09-12 09:36:02 -0400

    final commit

--- api/controllers/PessoaController.js
+++ api/controllers/PessoaController.js
@@ -13,7 +13,6 @@ class PessoaController {
   static getPessoaById = async (req, res) => {
     const { id } = req.params
     try {
-      //const pessoa = await database.Pessoas.findOne( { where: { id:Number(id) }} )
       const pessoa = await database.Pessoas.findByPk(id)
       res.status(200).json(pessoa)
     } catch (error) {
@@ -36,7 +35,7 @@ class PessoaController {
     try {
       await database.Pessoas.destroy({
         where: {
-          id: id
+          id: Number(id)
         }
       })
       res.status(200).json()
@@ -52,12 +51,86 @@ class PessoaController {
     try {
       await database.Pessoas.update(novasInfos, {
         where: {
-          id: id
+          id: Number(id)
         }
       })
 
       const pessoaAtualizada = await database.Pessoas.findByPk(id)
       res.status(200).json(pessoaAtualizada)
+    } catch (error) {
+        res.status(404).json(error.message)
+    }
+  }
+
+  static getMatriculaById = async (req, res) => {
+    const { estudanteId, matriculaId } = req.params
+    try {
+      const matricula = await database.Matriculas.findOne({
+        where: {
+          id: Number(matriculaId),
+          estudante_id: Number(estudanteId)
+        }
+      })
+      res.status(200).json(matricula)
+    } catch (error) {
+        res.status(500).json(error.message)
+    }
+  } 
+
+  static getMatriculasByPessoa = async (req, res) => {
+    const { estudanteId } = req.params
+    try {
+      const matricula = await database.Matriculas.findAll({
+        where: {
+          estudante_id: Number(estudanteId)
+        }
+      })
+      res.status(200).json(matricula)
+    } catch (error) {
+        res.status(500).json(error.message)
+    }
+  } 
+
+  static cadastraMatricula = async (req, res) => {
+    const { estudanteId } = req.params
+    const novaMatricula = { ...req.body, estudante_id: Number(estudanteId) }
+    try {
+      const matricula = await database.Matriculas.create(novaMatricula)  
+      res.status(200).json(matricula)
+    } catch (error) {
+      res.status(500).json(error.message)
+    }
+  }
+
+  static atualizaMatricula = async (req, res) => {
+    const { estudanteId, matriculaId } = req.params
+    const novasInfos = req.body
+
+    try {
+      await database.Matriculas.update(novasInfos, {
+        where: {
+          id: Number(matriculaId),
+          estudante_id: Number(estudanteId)
+        }
+      })
+
+      const matriculaAtualizada = await database.Matriculas.findByPk(matriculaId)
+      res.status(200).json(matriculaAtualizada)
+    } catch (error) {
+        res.status(404).json(error.message)
+    }
+  }
+
+  static deletaMatricula = async (req, res) => {
+    const { matriculaId, estudanteId } = req.params
+    try {
+      await database.Matriculas.destroy({
+        where: {
+          id: Number(matriculaId),
+          estudante_id: Number(estudanteId)
+        }
+      })
+      res.status(200).json()
     } catch (error) {
         res.status(404).json(error.message)
     }
--- api/routes/pessoasRoute.js
+++ api/routes/pessoasRoute.js
@@ -4,9 +4,17 @@ const PessoaController = require('../controllers/PessoaController')
 const router = Router()
 
 router.get('/pessoas', PessoaController.getPessoas)
+router.get('/pessoas/:estudanteId/matriculas', PessoaController.getMatriculasByPessoa)
+router.get('/pessoas/:estudanteId/matriculas/:matriculaId', PessoaController.getMatriculaById)
 router.get('/pessoas/:id', PessoaController.getPessoaById)
+
 router.post('/pessoas', PessoaController.cadastraPessoa)
+router.post('/pessoas/:estudanteId/matriculas', PessoaController.cadastraMatricula)
+
+router.delete('/pessoas/:estudanteId/matriculas/:matriculaId', PessoaController.deletaMatricula)
 router.delete('/pessoas/:id', PessoaController.deletaPessoa)
+
+router.put('/pessoas/:estudanteId/matriculas/:matriculaId', PessoaController.atualizaMatricula)
 router.put('/pessoas/:id', PessoaController.atualizaPessoa)
 
 module.exports = router
